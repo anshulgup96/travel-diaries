9c1391769ef71863f965297f112b07b0552ca759
Anshul Gupta <anshul.gup96@gmail.com>
2025-06-01 15:46:18 +0530

    Setup redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { store } from './store';
+import { Provider } from 'react-redux';
 import App from './App.jsx';
 
 import './main.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setPosts } from '../../reducers/postsReducer';
 
 import Navbar from '../../components/navbar/Navbar';
 
 function Feed() {
-  // useEffect(() => {
-  //   fetch('http://localhost:5050/users?name=Ned%20Stark', {
-  //     method: 'GET',
-  //     credentials: 'include',
-  //   })
-  //     .then((res) => res.json())
-  //     .then(console.log);
-  // }, []);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    fetch('http://localhost:5050/posts', {
+      method: 'GET',
+      credentials: 'include',
+    })
+      .then((res) => res.json())
+      .then((res) => dispatch(setPosts(res)));
+  }, [dispatch]);
   return (
     <div>
       <Navbar />
