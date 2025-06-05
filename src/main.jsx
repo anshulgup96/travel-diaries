5173f869b1589a927895bb02b6b9512ccaeafb71
Anshul Gupta <anshul.gup96@gmail.com>
2025-06-05 17:42:00 +0530

    Implemented login page

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import App from './App.jsx';
 
 import './main.css';
@@ -9,7 +11,9 @@ import './main.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <GoogleOAuthProvider clientId="441877564817-1spoqia6hqkaoeivqn3nqhjlbktke1q5.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
     </Provider>
   </StrictMode>
 );
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,73 @@
 import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+import { Input } from 'antd';
+import { Button } from 'antd';
+import { useForm, Controller } from 'react-hook-form';
+
+import styles from './Login.module.css';
 
 function Login() {
-  return <div>Login</div>;
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className={styles.loginContainer}>
+      <div>
+        <img
+          className={styles.heroImg}
+          src="login-page-background.jpg"
+          alt="login background"
+        />
+        <h1>Join a community of travellers, and plan your next vacation!</h1>
+      </div>
+      <div className={styles.formContainer}>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+          useOneTap
+        />
+        <p>OR</p>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.loginForm}>
+          <Controller
+            name="username"
+            control={control}
+            rules={{ required: 'The username is required' }}
+            render={({ field }) => (
+              <Input
+                {...field}
+                status={errors.username?.message ? 'error' : ''}
+                placeholder="Enter username"
+              />
+            )}
+          />
+          <p>{errors.username?.message}</p>
+          <Controller
+            name="password"
+            control={control}
+            rules={{ required: 'The password is required' }}
+            render={({ field }) => (
+              <Input.Password
+                {...field}
+                placeholder="Enter password"
+                status={errors.password?.message ? 'error' : ''}
+              />
+            )}
+          />
+          <p>{errors.password?.message}</p>
+          <Button htmlType="submit">Login</Button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
